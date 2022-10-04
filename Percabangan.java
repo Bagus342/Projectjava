a6b686a6c8df7864dc07229791e0a948d6adf0e9
Bagus342 <bagusyes005@gmail.com>
2022-10-04 15:29:13 +0700

    add repo new

---
+++ Percabangan.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Percabangan {
+
+    public static void main(String[] args) {
+        int angka;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan Angka : ");
+        angka = input.nextInt();
+        if (angka % 2 == 0)
+        {
+            System.out.println("Bilangan Genap");
+        }else{
+            System.out.println("Bilangan Ganjil");
+        }
+        
+        if (angka % 2 == 0)
+        {
+            if (angka > 10)
+            {
+                System.out.println("Bilangan Genap dan");
+                System.out.println("Merupakan Bilangan le");
+            }
+        }
+        String hasil = angka%2==0 ? "Bilangan Genap" + " halo angka " + angka : "Bilangan Ganjil";
+        System.out.println("Maka : " + hasil);
+    }
+    
+}
